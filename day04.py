64e1046d4c4c22216fea9ff33d63d43fcf3d2f1f
Karl Barrus <klbarrus@gmail.com>
2017-12-25 17:08:40 -0600

    day04

---
+++ day04.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# AdventOfCode 2015 Day 04
+
+import hashlib
+key = "ckczppom"
+done = False
+trail = 0
+N = 6 # leading 0's to look for
+while not done:
+    coin = key + str(trail)
+    m = hashlib.md5()
+    m.update(coin.encode('utf-8'))
+    dig = m.hexdigest()
+    lead = str(dig)[0:N]
+#    print("digest is {}".format(dig))
+#    print("lead chars: {}".format(lead))
+    if lead == "0"*N:
+        done = True
+    else:
+        trail += 1
+    
+print("Number: {}".format(trail))
