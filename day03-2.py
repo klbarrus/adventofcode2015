1d637e917c88fe98812ae19f6536915ebca1a499
Karl Barrus <klbarrus@gmail.com>
2017-12-24 21:27:19 -0600

    day03 2nd half

---
+++ day03-2.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+#AdventOfCode 2015 Day 03
+
+import sys
+
+loc = [0] * 4 # 0,1 = santa; 2,3 = robo
+robo = 0 # 0 = santa, 2 = robo
+grid = {}
+grid[(loc[0],loc[1])] = 1 # deliver to starting house
+with open(sys.argv[1]) as f:
+    for line in f:
+        for ch in line:
+            if ch == '^':
+                loc[1 + robo] += 1
+            elif ch == '<':
+                loc[0 + robo] -= 1
+            elif ch == '>':
+                loc[0 + robo] += 1
+            elif ch == 'v':
+                loc[1 + robo] -= 1
+            else:
+                print("unknown direction")
+
+            if (loc[0 + robo], loc[1 + robo]) in grid:
+                grid[(loc[0 + robo], loc[1 + robo])] += 1 # previously visited
+            else:
+                grid[(loc[0 + robo], loc[1 + robo])] = 1 # new home
+
+            if robo == 0: # switch off
+                robo = 2
+            else:
+                robo = 0
+f.close()
+
+num = len(list(grid.keys()))
+print("Num houses: {}".format(num))
