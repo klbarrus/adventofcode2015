86ed886206b4f186f03bcc289886cf7ac49a01bd
Karl Barrus <klbarrus@yahoo.com>
2018-01-27 23:29:48 -0500

    day 11

--- day11.py
+++ day11.py
@@ -3,7 +3,7 @@
 
 import sys
 
-def inc_str(istr):
+def next_str(istr):
     iarr = list(istr)
     cind = -1
     done = False
@@ -20,7 +20,6 @@ def inc_str(istr):
             if cind < -len(istr):
                 done = True
                 iarr.insert(0,'a')
-                
     ostr = ''.join(iarr)
     return ostr
 
@@ -43,9 +42,40 @@ def search_iol(istr):
             break
     return ret
 
-orig = sys.argv[1]
+def search_doubles(istr):
+    ret = False
+    p1ch = ""
+    p2ch = ""
+    p1pos = 0
+    p2pos = 0
+    for x in range(len(istr)-1):
+        if istr[x] == istr[x+1]:
+            if p1ch == "":
+                p1ch = istr[x]
+                p1pos = x
+            else:
+                p2ch = istr[x]
+                p2pos = x
+        if p1ch != "" and p2ch != "":
+            if p1ch != p2ch:
+                ret = True
+                break   # found 2 different pairs
+            elif p2pos != p1pos+1:
+                ret = True
+                break   # same pair but no overlap
+    return ret
 
-originc = inc_str(orig)
-print("{}".format(originc))
-print("iol found: {}".format(search_iol(originc)))
-print("straight: {}".format(search_straight(originc)))
+password = sys.argv[1]
+
+done = False
+while not done:
+    b1 = search_iol(password)
+    b2 = search_straight(password)
+    b3 = search_doubles(password)
+
+    if not b1 and b2 and b3:
+        break
+    else:
+        password = next_str(password)
+
+print("password: {}".format(password))
