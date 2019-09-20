bf4fdc3eeac7ef4afeeb81179dac1ab7fd28eff1
Karl Barrus <klbarrus@yahoo.com>
2019-09-20 19:00:16 -0400

    fix permissions, shebang

--- 2015-16.py
+++ 2015-16.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # AdventOfCode 2015 Day 16
 
 import sys
--- 2015-21.py
+++ 2015-21.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # AdventOfCode 2015 Day 21
 
 import itertools
